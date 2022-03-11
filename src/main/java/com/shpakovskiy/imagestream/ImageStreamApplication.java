135f85b0afa22d1ed7c3a1ef7c8673416d4a012c
ashpakovskiy <50966785+AndreiShpakovskiy@users.noreply.github.com>
2022-03-11 08:59:40 +0300

    Initial commit

---
+++ src/main/java/com/shpakovskiy/imagestream/ImageStreamApplication.java
@@ -0,0 +1,11 @@
+package com.shpakovskiy.imagestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStreamApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ImageStreamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shpakovskiy/imagestream/ImageStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shpakovskiy.imagestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
